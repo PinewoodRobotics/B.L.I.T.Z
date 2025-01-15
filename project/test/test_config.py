139d0924d0f9c8a9ab7c72ff4c58d0ab2db851cd
brigerodev <golden.notch.creeper@gmail.com>
2025-01-14 19:52:58 -0800

    remove unused tests

--- project/test/test_config.py
+++
@@ -1,8 +0,0 @@
-NATS_PORT = 4222
-PROFILER_CONFIG = {
-    "activated": True,
-    "profile-function": False,
-    "time-function": True,
-    "output-file": "profiler.txt",
-    "do-print": True,
-}
