a6e1615e9169268626d0febeed9d6fa66bbbe16a
brigerodev <golden.notch.creeper@gmail.com>
2025-03-04 10:16:58 -0800

    remove camera calib + some other outdated and useless

--- helper/camera_calibration/src/config/camera.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class CameraConfig(BaseModel):
-    camera_port: int
--- helper/camera_calibration/src/module/module_thread.py
+++
@@ -1,8 +0,0 @@
-from threading import Thread
-
-
-class ModuleThread(Thread):
-    is_running: bool = False
-
-    def stop(self):
-        self.is_running = False
