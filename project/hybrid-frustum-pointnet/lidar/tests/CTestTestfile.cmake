ad1810d49d77f2f49e2b0bcb9c22b43db83b4f3f
godbrigero <denis.koterov@gmail.com>
2025-03-15 00:11:20 -0700

    init rust

--- project/hybrid-frustum-pointnet/lidar/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/tests
-# Build directory: /mnt/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(RunAllTests "/mnt/tests/run_tests")
-set_tests_properties(RunAllTests PROPERTIES  _BACKTRACE_TRIPLES "/mnt/tests/CMakeLists.txt;32;add_test;/mnt/tests/CMakeLists.txt;0;")
