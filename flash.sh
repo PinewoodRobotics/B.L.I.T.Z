f06bd11145afbcb66bc1c3b10d3401d630bdd3ef
brigerodev <golden.notch.creeper@gmail.com>
2025-09-20 23:11:29 -0700

    update flash.sh for better impl and add a testing env docker for testing flashing

--- flash.sh
+++ flash.sh
@@ -29,4 +29,7 @@ cd ~/Documents/
 git clone https://github.com/PinewoodRobotics/B.L.I.T.Z.git
 cd B.L.I.T.Z
 
+# install git submodules
+git submodule update --init --recursive
+
 bash scripts/install.sh --name "$name"
