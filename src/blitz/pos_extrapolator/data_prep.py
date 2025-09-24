hopefully fix rotation issues (maybe not though)

src/blitz/pos_extrapolator/data_prep.py
@@ -29,7 +29,7 @@ class DataPreparer(Protocol[T, C]):
 
     def prepare_input(
         self, data: T, sensor_id: str, x: NDArray[np.float64] | None = None
-    ) -> KalmanFilterInput: ...
+    ) -> KalmanFilterInput | None: ...
 
     def get_data_type(self) -> type[T]: ...
 
@@ -54,11 +54,11 @@ class DataPreparerManager:
 
     def prepare_data(
         self, data: object, sensor_id: str, x: NDArray[np.float64] | None = None
-    ) -> KalmanFilterInput:
+    ) -> KalmanFilterInput | None:
         data_type_name = type(data).__name__
 
         if data_type_name not in self._registry:
-            raise ValueError(f"No preparer registered for data type: {data_type_name}")
+            return None
 
         preparer_class = self._registry[data_type_name]
         config_instance = self._config_instances.get(data_type_name)

src/blitz/pos_extrapolator/preparers/AprilTagPreparer.py
@@ -9,6 +9,7 @@ from blitz.common.util.math import (
     get_np_from_vector,
     get_robot_in_world,
     get_translation_rotation_components,
+    make_3d_rotation_from_yaw,
     make_transformation_matrix_p_d,
     transform_matrix_to_size,
     transform_vector_to_size,
@@ -81,7 +82,7 @@ class AprilTagDataPreparer(DataPreparer[AprilTagData, AprilTagDataPreparerConfig
 
     def prepare_input(
         self, data: AprilTagData, sensor_id: str, x: NDArray[np.float64] | None = None
-    ) -> KalmanFilterInput:
+    ) -> KalmanFilterInput | None:
         if data.WhichOneof("data") == "raw_tags":
             raise ValueError("Tags are not in processed format")
 
@@ -106,16 +107,24 @@ class AprilTagDataPreparer(DataPreparer[AprilTagData, AprilTagDataPreparerConfig
                 ),
             )
 
+            tag_in_camera_rotation = make_3d_rotation_from_yaw(tag.rotationWPILib.yaw)
+
+            '''
             tag_in_camera_rotation = (
                 PositionExtrapolator.CAMERA_OUTPUT_TO_ROBOT_ROTATION
                 @ from_float_list(list(tag.pose_R), 3, 3)
                 @ PositionExtrapolator.CAMERA_OUTPUT_TO_ROBOT_ROTATION.T
             )
+            '''
 
+            tag_in_camera_pose = np.array([tag.positionWPILib.x, tag.positionWPILib.y, tag.positionWPILib.z])
+
+            '''
             tag_in_camera_pose = (
                 PositionExtrapolator.CAMERA_OUTPUT_TO_ROBOT_ROTATION
                 @ np.array(tag.pose_t)
             )
+            '''
 
             T_tag_in_camera = create_transformation_matrix(
                 rotation_matrix=tag_in_camera_rotation,
@@ -146,7 +155,7 @@ class AprilTagDataPreparer(DataPreparer[AprilTagData, AprilTagDataPreparerConfig
             datapoint = [
                 render_pose[0],
                 render_pose[1],
-                render_direction_vector[1],
+                render_direction_vector[1], # TODO: fix this bit if wrong.
                 render_direction_vector[0],
             ]
             

src/blitz/pos_extrapolator/preparers/ImuDataPreparer.py
@@ -45,7 +45,7 @@ class ImuDataPreparer(DataPreparer[ImuData, ImuDataPreparerConfig]):
 
     def prepare_input(
         self, data: ImuData, sensor_id: str, x: NDArray[np.float64] | None = None
-    ) -> KalmanFilterInput:
+    ) -> KalmanFilterInput | None:
         config = self.config.config[sensor_id]
         values: list[float] = []
 

src/blitz/pos_extrapolator/preparers/OdomDataPreparer.py
@@ -46,7 +46,7 @@ class OdomDataPreparer(DataPreparer[OdometryData, OdomDataPreparerConfig]):
 
     def prepare_input(
         self, data: OdometryData, sensor_id: str, x: NDArray[np.float64] | None = None
-    ) -> KalmanFilterInput:
+    ) -> KalmanFilterInput | None:
         values: list[float] = []
         if self.config.use_position:
             values.append(data.position.position.x)