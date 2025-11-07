e64d49992d74eb16890b1dc511caea9304ec92bc
brigerodev <golden.notch.creeper@gmail.com>
2025-11-07 14:08:36 -0800

    update discovery to provide much more info

--- watchdog/discovery.py
+++ watchdog/discovery.py
@@ -2,7 +2,15 @@ import socket
 from zeroconf import ServiceInfo, Zeroconf
 
 from watchdog.util.logger import success
-from watchdog.util.system import get_local_hostname, get_local_ip
+from watchdog.util.system import (
+    get_local_hostname,
+    get_local_ip,
+    get_system_name,
+    load_basic_system_config,
+)
+
+
+TYPE_ = "_watchdog._udp.local."
 
 
 def enable_discovery():
@@ -11,18 +19,26 @@ def enable_discovery():
 
     hostname = socket.gethostname()
     hostname_local = get_local_hostname()
+    system_name = get_system_name()
     local_ip = get_local_ip("eth0") or get_local_ip("en0") or "127.0.0.1"
+    system_config = load_basic_system_config()
 
     addresses = [socket.inet_aton(local_ip)]
 
     _info = ServiceInfo(
-        "_deploy._udp.local.",
-        f"{hostname}._deploy._udp.local.",
+        TYPE_,
+        f"{hostname}.{TYPE_}",
         addresses=addresses,
         port=9999,
         server=hostname_local,
-        properties={"id": hostname, "hostname": hostname_local},
+        properties={
+            "hostname": hostname,
+            "hostname_local": hostname_local,
+            "system_name": system_name,
+            "watchdog_port": system_config.watchdog.port,
+            "autobahn_port": system_config.autobahn.port,
+        },
     )
 
     zeroconf.register_service(_info)
-    success(f"Registered service {hostname} on {hostname_local} ({local_ip})")
+    success(f"Registered service {system_name} on {hostname_local} ({local_ip})")
